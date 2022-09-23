f516b7d60c2c60eb5a5ebd8b30703f7b9d88142f
Martin Suarez <martin086@gmail.com>
2022-09-23 17:21:16 -0300

    Programamos funcionalidad botón Add to Cart.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 // Cambio de cantidad de ítems ingresado por el usuario.
-
-let minusBtn = document.querySelector('.input__minus');
-let plusBtn = document.querySelector('.input__plus');
+const minusBtn = document.querySelector('.input__minus');
+const plusBtn = document.querySelector('.input__plus');
 let userInput = document.querySelector('.input__number');
 
 let userInputNumber = 0;
@@ -20,4 +19,19 @@ minusBtn.addEventListener('click', ()=>{
     userInput.value = userInputNumber;
     console.log(userInputNumber);
 });
+
+// Agregar el total de productos al carrito cuando se presiona el botón ADD TO CART
+const addToCartBtn = document.querySelector('.details__button');
+let cartNotification = document.querySelector('.header__cart--notification');
+
+addToCartBtn.addEventListener('click', ()=>{
+    let lastValue = parseInt(cartNotification.innerText);
+    lastValue = lastValue + userInputNumber;
+
+    cartNotification.innerText = userInputNumber;
+    cartNotification.style.display = 'block';
+});
+
+
+
 
