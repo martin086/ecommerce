450331e76426f242555d1a473ea8bc50268930d9
Martin Suarez <martin086@gmail.com>
2022-09-23 16:55:51 -0300

    Programamos cambio en cantidad de items al presionar botones.

---
+++ main.js
@@ -0,0 +1,23 @@
+// Cambio de cantidad de ítems ingresado por el usuario.
+
+let minusBtn = document.querySelector('.input__minus');
+let plusBtn = document.querySelector('.input__plus');
+let userInput = document.querySelector('.input__number');
+
+let userInputNumber = 0;
+
+plusBtn.addEventListener('click', ()=>{
+    userInputNumber++;
+    userInput.value = userInputNumber;
+    console.log(userInputNumber);
+});
+
+minusBtn.addEventListener('click', ()=>{
+    userInputNumber--;
+    if(userInputNumber <= 0) {
+        userInputNumber = 0;
+    }
+    userInput.value = userInputNumber;
+    console.log(userInputNumber);
+});
+
